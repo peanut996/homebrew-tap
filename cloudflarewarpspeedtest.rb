323eb600364ee9b0d2ae00b973defd2401929bfc
peanut996 <849421294godw@gmail.com>
2023-10-21 23:39:33 +0800

    build with version

--- cloudflarewarpspeedtest.rb
+++ cloudflarewarpspeedtest.rb
@@ -5,6 +5,7 @@ class Cloudflarewarpspeedtest < Formula
   homepage "https://github.com/peanut996/CloudflareWarpSpeedTest"
   url "https://github.com/peanut996/CloudflareWarpSpeedTest/archive/refs/tags/v1.1.4.tar.gz"
   license "GPL V3"
+  sha256 "b0a06717290a3f3bbef06857d9767efac4ea26df17282c17156b874feb75fec9"
 
   depends_on "go@1.20" => :build
 
@@ -13,7 +14,7 @@ class Cloudflarewarpspeedtest < Formula
     path = buildpath/"src/github.com/peanut996/CloudflareWarpSpeedTest"
     path.install Dir["*"]
     cd path do
-      system "go", "build", "-o", "#{bin}/CloudflareWarpSpeedTest"
+      system "go", "build", "-ldflags", "-X main.Version=#{version}", "-o", "bin/CloudflareWarpSpeedTest"
     end
   end
 
