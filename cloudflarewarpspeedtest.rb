5c9fe2d91bfc44076aabf3e54f9bbc11f58f8349
peanut996 <849421294godw@gmail.com>
2023-11-09 10:50:54 +0800

    fix sha256

--- cloudflarewarpspeedtest.rb
+++ cloudflarewarpspeedtest.rb
@@ -2,7 +2,7 @@ class Cloudflarewarpspeedtest < Formula
   desc "Speedtest tool for Cloudflare Warp"
   homepage "https://github.com/peanut996/CloudflareWarpSpeedTest"
   url "https://github.com/peanut996/CloudflareWarpSpeedTest/archive/refs/tags/v1.1.6.tar.gz"
-  sha256 "82d50e9e35c38ff9551250abddf84c4f71f06787"
+  sha256 "7a62a693120e9f0e6207ec9640349c88a01eafaaca3884831d2d596ee6c7edfe"
   license "GPL-3.0-only"
 
   depends_on "go@1.20" => :build
