3a4c90d6825d7aa75961b5f20910a054628d2218
peanut996 <849421294godw@gmail.com>
2024-06-19 11:39:15 +0800

    New version: peanut996.CloudflareWarpSpeedTest version 1.4.3

--- cloudflarewarpspeedtest.rb
+++ cloudflarewarpspeedtest.rb
@@ -1,8 +1,8 @@
 class Cloudflarewarpspeedtest < Formula
   desc "Speedtest tool for Cloudflare Warp"
   homepage "https://github.com/peanut996/CloudflareWarpSpeedTest"
-  url "https://github.com/peanut996/CloudflareWarpSpeedTest/archive/refs/tags/v1.4.2.tar.gz"
-  sha256 "dbf9e0d5a4e07f6872f69293b15518a23feea8987daf6326fbd8bc47f3366a7c"
+  url "https://github.com/peanut996/CloudflareWarpSpeedTest/archive/refs/tags/v1.4.3.tar.gz"
+  sha256 "4c5f4583d9f06faec11c82271fc7e90af09feccddbb9e1700a334faae60de2f9"
   license "GPL-3.0-only"
 
   depends_on "go@1.20" => :build
@@ -12,7 +12,7 @@ class Cloudflarewarpspeedtest < Formula
     path = buildpath/"src/github.com/peanut996/CloudflareWarpSpeedTest"
     path.install Dir["*"]
     cd path do
-      system "go", "build", "-ldflags", "-X main.Version=v1.4.2", "-o", "#{bin}/CloudflareWarpSpeedTest"
+      system "go", "build", "-ldflags", "-X main.Version=v1.4.3", "-o", "#{bin}/CloudflareWarpSpeedTest"
     end
   end
 end
